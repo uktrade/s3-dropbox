e7bb1681f53e9f412c77a335a3787c3fefdbe77c
Michal Charemza <michal@charemza.name>
2023-02-18 07:28:00 +0000

    tests: fail test quicker if process has exited

--- test_main.py
+++ test_main.py
@@ -10,14 +10,16 @@ import pytest
 
 @pytest.fixture
 def app():
-    def wait_until_connectable(port, max_attempts=1000):
+    def wait_until_connectable(p, port, max_attempts=1000):
         for i in range(0, max_attempts):
             try:
                 with socket.create_connection(('127.0.0.1', port), timeout=0.1):
                     break
             except (OSError, ConnectionRefusedError):
-                if i == max_attempts - 1:
+                p.poll()
+                if p.returncode is not None or i == max_attempts - 1:
                     raise
+
                 time.sleep(0.01)
 
     with open('Procfile', 'r') as f:
@@ -25,7 +27,7 @@ def app():
 
     command = next((command for (name, _, command) in lines if command if name == 'web'))
     with subprocess.Popen(shlex.split(command), env={**os.environ, 'PORT': '8888'}) as p:
-        wait_until_connectable(8888)
+        wait_until_connectable(p, 8888)
         yield p
         p.terminate()    
     p.kill()
