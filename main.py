9d539dde28a12e151f3c4294cc8d4b1ad20d80d5
Michal Charemza <michal@charemza.name>
2023-02-18 07:38:35 +0000

    refactor: dummy sync upload function
    This is to just check we can do boto3 uploads

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
+from starlette.concurrency import run_in_threadpool
 
 app = FastAPI()
 
 
 @app.get("/v1/drop")
 async def drop():
+
+    def upload():
+        pass
+    await run_in_threadpool(upload)
+
     return JSONResponse(status_code=status.HTTP_201_CREATED, content={})
