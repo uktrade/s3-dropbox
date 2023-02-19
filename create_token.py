09da0d674c6db4cfdce5609b49e2a999c8927269
Michal Charemza <michal@charemza.name>
2023-02-19 12:12:41 +0000

    style: tabs to spaces

--- create_token.py
+++ create_token.py
@@ -4,19 +4,19 @@ import secrets
 
 
 def create_token():
-	token_client = secrets.token_urlsafe(64)
-	salt = secrets.token_urlsafe(64)
-	n = 16384
-	r = 8
-	p = 1
-	dklen = 64
-	hashed_and_salted_bytes = hashlib.scrypt(token_client.encode('ascii'), salt=salt.encode('ascii'), n=n, r=r, p=p, dklen=dklen)
-	hashed_and_salted = base64.b64encode(hashed_and_salted_bytes).decode('ascii')
+    token_client = secrets.token_urlsafe(64)
+    salt = secrets.token_urlsafe(64)
+    n = 16384
+    r = 8
+    p = 1
+    dklen = 64
+    hashed_and_salted_bytes = hashlib.scrypt(token_client.encode('ascii'), salt=salt.encode('ascii'), n=n, r=r, p=p, dklen=dklen)
+    hashed_and_salted = base64.b64encode(hashed_and_salted_bytes).decode('ascii')
 
-	return token_client, f'{n}|{r}|{p}|{dklen}|{salt}|{hashed_and_salted}'
+    return token_client, f'{n}|{r}|{p}|{dklen}|{salt}|{hashed_and_salted}'
 
 
 if __name__ == '__main__':
-	token_client, token_server = create_token()
-	print(f'Plain text Bearer token to give to client: {token_client}')
-	print(f'TOKEN to store in server:                  {token_server}')
+    token_client, token_server = create_token()
+    print(f'Plain text Bearer token to give to client: {token_client}')
+    print(f'TOKEN to store in server:                  {token_server}')
